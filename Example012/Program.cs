06e237356013067f7a1c8da3f0acbba5a4ef2da0
Giovanni2810 <V_crazy28@mail.ru>
2023-11-05 16:30:45 +0300

    added new folder

---
+++ Example012/Program.cs
@@ -0,0 +1,63 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапохон возможных координат точек в этой четверти (X и Y)
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string[] msg = {
+    "X>0, Y>0", // 0
+    "X<0, Y>0", // 1
+    "X<0, Y<0", // 2
+    "X>0, Y<0", // 3
+    "Такой четверти нет" // 4
+};
+
+if(number > 0 && number < 5)
+    System.Console.WriteLine(msg[number - 1]);
+else
+    System.Console.WriteLine(msg[4]);
+
+// 1 - > 0
+// 2 ->  1
+// 3 - > 2
+// 4 - > 3
+
+
+// switch (number)
+// {
+//     case 1:
+//         System.Console.WriteLine("X>0, Y>0");
+//         break;
+//     case 2:
+//         System.Console.WriteLine("X<0, Y>0");
+//         break;
+//     case 3:
+//         System.Console.WriteLine("X<0, Y<0");
+//         break;
+//     case 4:
+//         System.Console.WriteLine("X>0, Y<0");
+//         break;
+//     default:
+//         System.Console.WriteLine("Такой четверти нет");
+//         break;
+// }
+
+// if (number == 1){
+//     System.Console.WriteLine("X>0, Y>0");
+// }
+// else if (number == 2){
+//     System.Console.WriteLine("X<0, Y>0");
+// }
+// else if (number == 3)
+// {
+//     System.Console.WriteLine("X<0, Y<0");
+// }
+
+// else if (number == 4)
+// {
+//     System.Console.WriteLine("X>0, Y<0");
+// }
+// else{
+//     System.Console.WriteLine("Такой четверти нет");
+// }
+
+
---
+++ Examples011/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача Nº17. Напишите программу, которая принимает на вход координаты точки (Х и Y). 
+// причем Х ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// int[] coords = new int[2];
+
+// System.Console.WriteLine("Введите координату Х: ");
+// coords[0] = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Введите координату Y: ");
+// coords[1] = Convert.ToInt32(Console.ReadLine());
+
+// string[] msg = {
+//     "Точка находится в первой четверти"
+//     "Точка находится во второй четверти" 
+//     "Точка находится в третьей четверти" 
+//     "Точка находится в четвертой четверти" 
+//     "Одна из координат равна нулю"
+//     };
+
+// if (coords[0] > 0 && coords[1] > 0)
+
+//     System.Console.WriteLine(msg[0]);
+
+// else if (coords[0] < 0 && coords[1] > 0)
+
+//     System.Console.WriteLine(msg[1]);
+
+// else if (coords[0] < 0 && coords[1] < 0)
+
+//     System.Console.WriteLine(msg[2]);
+
+// else if (coords[0] > 0 && coords[1] < 0)
+
+//     System.Console.WriteLine(msg[3]);
+
+// else
+
+//     System.Console.WriteLine(msg[4]);
+
+
